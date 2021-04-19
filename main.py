2a665bafe68fcf153cca879f05d31a6970aa9643
KubaBoi <jakubanderle@outlook.cz>
2021-04-20 01:47:12 +0200

    getting devices

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from subprocess import call, check_output
-output = check_output(['hcitool', 'con']).decode("utf-8") 
-lines = output.split(">")
 
-devices = []
-for d in range(1, len(lines)):
-    mac = lines[d].strip().split(" ")[1] #ziska mac adresu
-    name = check_output(["hcitool", "name", mac]).decode("utf-8").rstrip()
-    devices.append((mac, name))
+def getDevices():
+    output = check_output(['hcitool', 'con']).decode("utf-8") 
+    lines = output.split(">")
 
-for i in devices:
-    print(i)
+    devices = []
+    for d in range(1, len(lines)):
+        mac = lines[d].strip().split(" ")[1] #ziska mac adresu
+        name = check_output(["hcitool", "name", mac]).decode("utf-8").rstrip() #ziska jmeno zarizeni
+        devices.append((name, mac))
+
+    return devices
