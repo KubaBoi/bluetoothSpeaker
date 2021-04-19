986c3349aa6b0f4b2ad2fd2ccf3253e3622b0342
KubaBoi <jakubanderle@outlook.cz>
2021-04-20 01:44:19 +0200

    debug

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ lines = output.split(">")
 devices = []
 for d in range(1, len(lines)):
     mac = lines[d].strip().split(" ")[1] #ziska mac adresu
-    name = check_output(["hcitool", "name", mac]).decode("utf-8")
+    name = check_output(["hcitool", "name", mac]).decode("utf-8").rstrip()
     devices.append((mac, name))
 
 for i in devices:
